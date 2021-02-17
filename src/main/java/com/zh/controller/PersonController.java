cfd9e31772258506deb5efd6ec26ee047640512f
${zhang0415} <${1585222@139.com}>
2021-02-17 22:33:25 +0800

    这还是二次测试

--- src/main/java/com/zh/controller/PersonController.java
+++ src/main/java/com/zh/controller/PersonController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PersonController {
 
@@ -14,6 +17,18 @@ public class PersonController {
         Person person = new Person("张三","12");
         mv.setViewName("welcome");
         mv.addObject("person",person);
+        mv.addObject("mes","zh");
+        mv.addObject("num",3);
+        Person person2 = new Person("张4","13");
+        Person person3 = new Person("张5","14");
+        Person person4 = new Person("张6","15");
+        Person person5 = new Person("张7","16");
+        ArrayList<Person> list = new ArrayList<>();
+        list.add(person2);
+        list.add(person3);
+        list.add(person4);
+        list.add(person5);
+        mv.addObject("list",list);
         return mv;
     }
 }
