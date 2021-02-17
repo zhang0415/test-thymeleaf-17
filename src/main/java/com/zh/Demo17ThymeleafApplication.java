f1189731f8f061e73bd90e287d3bd21876addf5d
${zhang0415} <${1585222@139.com}>
2021-02-17 16:34:46 +0800

    第一次测试提交

---
+++ src/main/java/com/zh/Demo17ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.zh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo17ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo17ThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zh/Demo17ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo17ThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
